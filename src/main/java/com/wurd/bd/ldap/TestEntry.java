515f149d9b979b569322fcc888ae083805016664
wu <wurd.email@gmail.com>
2021-11-12 01:03:48 +0800

    ldap

--- src/main/java/com/wurd/bd/ldap/TestEntry.java
+++
@@ -1,9 +0,0 @@
-package com.wurd.bd.ldap;
-
-import lombok.ToString;
-import org.springframework.ldap.odm.annotations.Entry;
-
-@Entry(base = "ou=Policies,dc=wurd,dc=com", objectClasses = {"organizationalRole","pwdPolicy"})
-@ToString
-public class TestEntry {
-}
