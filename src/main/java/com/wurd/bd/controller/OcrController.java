6d422f0c3f1f2d3b6ce0d21794fc10a1b673c131
wurd <wurd.email@gmail.com>
2021-12-02 18:06:38 +0800

    update

--- src/main/java/com/wurd/bd/controller/OcrController.java
+++ src/main/java/com/wurd/bd/controller/OcrController.java
@@ -3,6 +3,7 @@ package com.wurd.bd.controller;
 import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 import com.wurd.bd.Utils.CommonUtils;
 import com.wurd.bd.constants.Config;
 import com.wurd.bd.exception.CommonException;
@@ -41,7 +42,7 @@ public class OcrController {
     private String paddle_t = "paddle";
 
     @PostMapping("/get/policy")
-    String getTestFromPic_policy(MultipartFile file, String flag, String type) throws CommonException {
+    String getTestFromPic_policy(MultipartFile file, String flag, String type, String html) throws CommonException {
         try {
             check(file);
             String fileName = file.getOriginalFilename();
@@ -50,7 +51,7 @@ public class OcrController {
             fileName = System.currentTimeMillis() + "_" + fileName;
             String outFilePath = config.getEasyOcrPicturePath() + File.separator + fileName;
             CommonUtils.writeFile2Disk(fileStream, outFilePath);
-            String uri = null;
+            String uri;
             if (paddle_t.equalsIgnoreCase(type)) {
                 uri = "http://127.0.0.1:5001/paddle-ocr?path="
                         + URLEncoder.encode(outFilePath, StandardCharsets.UTF_8.toString());
@@ -63,13 +64,7 @@ public class OcrController {
             int status = response.getStatusLine().getStatusCode();
             String result = EntityUtils.toString(response.getEntity(), StandardCharsets.UTF_8.toString());
             if (HttpStatus.SC_OK == status) {
-                JsonArray json = gson.fromJson(result, JsonArray.class);
-                if ("1".equals(flag)) {
-                    JsonArray array = formatResult(type, json);
-                    return array.toString();
-                } else {
-                    return json.toString();
-                }
+                return response(flag, type, html, result);
             } else {
                 log.error(result);
                 throw new CommonException("Ocr failed");
@@ -82,7 +77,7 @@ public class OcrController {
     }
 
     @PostMapping("/get")
-    String getTestFromPic(MultipartFile file, String flag, String type) throws CommonException {
+    String getTestFromPic(MultipartFile file, String flag, String type, String html) throws CommonException {
         try {
             check(file);
             String fileName = file.getOriginalFilename();
@@ -106,13 +101,7 @@ public class OcrController {
             processReturn(process, result, error);
             int state = process.waitFor();
             if (state == 0) {
-                JsonArray json = gson.fromJson(result.toString(), JsonArray.class);
-                if ("1".equals(flag)) {
-                    JsonArray array = formatResult(type, json);
-                    return array.toString();
-                } else {
-                    return json.toString();
-                }
+                return response(flag, type, html, result.toString());
             } else {
                 log.error(error.toString());
                 throw new CommonException("Ocr failed");
@@ -124,74 +113,125 @@ public class OcrController {
         }
     }
 
-    private JsonArray formatResult(String type, JsonArray json) {
+    private String response(String flag, String type, String html, String result) {
+        JsonArray json = gson.fromJson(result, JsonArray.class);
+        if ("true".equalsIgnoreCase(html)) {
+            String resHtml = format2Html(type, json);
+            return resHtml;
+        } else if ("1".equals(flag)) {
+            JsonArray array = format2Json(type, json);
+            return array.toString();
+        } else {
+            return json.toString();
+        }
+    }
+
+    private String format2Html(String type, JsonArray json) {
+        JsonArray d = formatResult(type, json);
+        StringBuilder sb = new StringBuilder();
+        sb.append("<html>")
+                .append("<head>")
+                .append("<meta http-equiv='content-type' content='text/html;charset=utf-8'>")
+                .append("</head>")
+                .append("<body>");
+        for (int i = 0; i < d.size(); i++) {
+            JsonArray pos = d.get(i).getAsJsonObject().get("pos").getAsJsonArray();
+            String text = d.get(i).getAsJsonObject().get("text").getAsString();
+            int x = pos.get(0).getAsInt();
+            int y = pos.get(1).getAsInt();
+            sb.append("<div style='")
+                    .append("position:absolute;")
+                    .append("top:").append(y).append("px;")
+                    .append("left:").append(x).append("px;")
+                    .append("'>")
+                    .append(text)
+                    .append("</div>");
+        }
+        sb.append("</body>")
+                .append("</html>");
+        return sb.toString();
+    }
+
+    private JsonArray format2Json(String type, JsonArray json) {
+        JsonArray d = formatResult(type, json);
         JsonArray array = new JsonArray();
+        for (int i = 0; i < d.size(); i++) {
+            JsonElement s = d.get(i).getAsJsonObject().get("text");
+            array.add(s);
+        }
+        return array;
+    }
+
+    private JsonArray formatResult(String type, JsonArray json) {
+        JsonArray d = new JsonArray();
         if (paddle_t.equalsIgnoreCase(type)) {
             JsonArray arr = json.get(0).getAsJsonObject().get("data").getAsJsonArray();
             for (int i = 0; i < arr.size(); i++) {
                 JsonElement s = arr.get(i).getAsJsonObject().get("text");
                 if (s != null && StringUtils.isNotEmpty(s.getAsString())) {
-                    array.add(s);
+                    JsonObject obj = new JsonObject();
+                    obj.add("pos", arr.get(i).getAsJsonObject()
+                            .get("text_box_position").getAsJsonArray().get(0));
+                    obj.add("text", s);
+                    d.add(obj);
                 }
             }
         } else {
             for (int i = 0; i < json.size(); i++) {
                 JsonElement s = json.get(i).getAsJsonArray().get(1);
                 if (s != null && StringUtils.isNotEmpty(s.getAsString())) {
-                    array.add(s);
+                    JsonObject obj = new JsonObject();
+                    obj.add("pos", json.get(i).getAsJsonArray()
+                            .get(0).getAsJsonArray().get(0));
+                    obj.add("text", s);
+                    d.add(obj);
                 }
             }
         }
-        return array;
+        return d;
     }
 
     private void processReturn(Process process, StringBuilder result, StringBuilder error) {
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                InputStream inputStream = null;
-                try {
-                    inputStream = process.getInputStream();
-                    BufferedReader reader =
-                            new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
-                    String line = null;
-                    while ((line = reader.readLine()) != null) {
-                        result.append(line);
-                    }
-                } catch (Exception e) {
-                    e.printStackTrace();
-                } finally {
-                    if (inputStream != null) {
-                        try {
-                            inputStream.close();
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
+        new Thread(() -> {
+            InputStream inputStream = null;
+            try {
+                inputStream = process.getInputStream();
+                BufferedReader reader =
+                        new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    result.append(line);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                if (inputStream != null) {
+                    try {
+                        inputStream.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
                     }
                 }
             }
         }).start();
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                InputStream errorStream = null;
-                try {
-                    errorStream = process.getErrorStream();
-                    BufferedReader reader =
-                            new BufferedReader(new InputStreamReader(errorStream, StandardCharsets.UTF_8));
-                    String line = null;
-                    while ((line = reader.readLine()) != null) {
-                        error.append(line);
-                    }
-                } catch (Exception e) {
-                    e.printStackTrace();
-                } finally {
-                    if (errorStream != null) {
-                        try {
-                            errorStream.close();
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
+        new Thread(() -> {
+            InputStream errorStream = null;
+            try {
+                errorStream = process.getErrorStream();
+                BufferedReader reader =
+                        new BufferedReader(new InputStreamReader(errorStream, StandardCharsets.UTF_8));
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    error.append(line);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                if (errorStream != null) {
+                    try {
+                        errorStream.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
                     }
                 }
             }
