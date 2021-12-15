eb9d19651fe963887c3dc95abfc9a7452b70f92a
wurd <wurd.email@gmail.com>
2021-12-15 15:23:20 +0800

    update

--- src/test/java/com/wurd/bd/internalTest2/Test2.java
+++ src/test/java/com/wurd/bd/internalTest2/Test2.java
@@ -10,7 +10,9 @@ import javax.naming.NamingException;
 import javax.naming.spi.InitialContextFactory;
 import javax.naming.spi.InitialContextFactoryBuilder;
 import javax.naming.spi.NamingManager;
+import java.util.HashMap;
 import java.util.Hashtable;
+import java.util.Map;
 
 public class Test2 {
     public static void main(String[] args) throws NamingException {
@@ -18,23 +20,33 @@ public class Test2 {
         Logger log = LogManager.getLogger(Test2.class.getName());
         log.error("${jndi:111}");
 
-//        NamingManager.setInitialContextFactoryBuilder(new InitialContextFactoryBuilder() {
-//            @Override
-//            public InitialContextFactory createInitialContextFactory(Hashtable<?, ?> environment) throws NamingException {
-//                return new InitialContextFactory() {
-//                    @Override
-//                    public Context getInitialContext(Hashtable<?, ?> environment) throws NamingException {
-//                        return new InitialContext(environment);
-//                    }
-//                };
-//            }
-//        });
+        NamingManager.setInitialContextFactoryBuilder(new InitialContextFactoryBuilder() {
+            @Override
+            public InitialContextFactory createInitialContextFactory(Hashtable<?, ?> environment) throws NamingException {
+                return new InitialContextFactory() {
+                    @Override
+                    public Context getInitialContext(Hashtable<?, ?> environment) throws NamingException {
+                        return new InitialContext() {
+                            private Map<String, Object> bindMap = new HashMap<String, Object>();
+
+                            public Object lookup(String name) throws NamingException {
+                                return bindMap.get(name);
+                            }
+
+                            public void bind(String name, Object obj) throws NamingException {
+                                bindMap.put(name, obj);
+                            }
+
+                        };
+                    }
+                };
+            }
+        });
 
         Hashtable<String, String> params = new Hashtable<>();
         params.put("test", "111");
-//        params.put("java.naming.factory.initial", "111");
         Context initial = new InitialContext(params);
-//        initial.bind("test", "111");
+        initial.bind("test", "222");
         Object obj = initial.lookup("test");
 
         System.out.println();
