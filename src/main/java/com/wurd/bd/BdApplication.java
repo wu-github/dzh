d0e669aa255d5e9fb6be27d1b7106a2386574802
wurd <wurd.email@gmail.com>
2021-12-15 14:02:44 +0800

    update

--- src/main/java/com/wurd/bd/BdApplication.java
+++ src/main/java/com/wurd/bd/BdApplication.java
@@ -1,15 +1,12 @@
 package com.wurd.bd;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@Slf4j
 @SpringBootApplication
 public class BdApplication {
 
 	public static void main(String[] args) {
-		log.error("${jndi:}");
 		SpringApplication.run(BdApplication.class, args);
 	}
 
--- src/test/java/com/wurd/bd/internalTest2/Test2.java
+++ src/test/java/com/wurd/bd/internalTest2/Test2.java
@@ -7,15 +7,35 @@ import org.apache.logging.log4j.Logger;
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
+import javax.naming.spi.InitialContextFactory;
+import javax.naming.spi.InitialContextFactoryBuilder;
+import javax.naming.spi.NamingManager;
+import java.util.Hashtable;
 
 public class Test2 {
     public static void main(String[] args) throws NamingException {
-        System.setProperty("log4j2.formatMsgNoLookups","true");
+        System.setProperty("log4j2.formatMsgNoLookups", "true");
         Logger log = LogManager.getLogger(Test2.class.getName());
         log.error("${jndi:111}");
 
-        Context initial = new InitialContext();
-        initial.lookup("java:");
+//        NamingManager.setInitialContextFactoryBuilder(new InitialContextFactoryBuilder() {
+//            @Override
+//            public InitialContextFactory createInitialContextFactory(Hashtable<?, ?> environment) throws NamingException {
+//                return new InitialContextFactory() {
+//                    @Override
+//                    public Context getInitialContext(Hashtable<?, ?> environment) throws NamingException {
+//                        return new InitialContext(environment);
+//                    }
+//                };
+//            }
+//        });
+
+        Hashtable<String, String> params = new Hashtable<>();
+        params.put("test", "111");
+//        params.put("java.naming.factory.initial", "111");
+        Context initial = new InitialContext(params);
+//        initial.bind("test", "111");
+        Object obj = initial.lookup("test");
 
         System.out.println();
 
